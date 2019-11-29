a4877b0ba0e4d1ab7ab307346237d1a42107ffad
insanetim <insanetim1986@gmail.com>
2019-11-29 10:14:00 +0200

    add build

---
+++ build/precache-manifest.b488c0032a61db793c31375c0f0eecb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99832198eabaeabb21717c7763ef721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd31d8df4ff9a957a06a",
+    "url": "/static/css/main.8435651a.chunk.css"
+  },
+  {
+    "revision": "2c10bb5d0806e78d8282",
+    "url": "/static/js/2.8aff3026.chunk.js"
+  },
+  {
+    "revision": "fd31d8df4ff9a957a06a",
+    "url": "/static/js/main.ea116aef.chunk.js"
+  },
+  {
+    "revision": "74f081441302f3122ef8",
+    "url": "/static/js/runtime~main.184138dc.js"
+  }
+]);
